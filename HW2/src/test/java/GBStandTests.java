4cf6c0640f13e233e537ad04824c05793177262b
Dm1mg <143843122+Dm1mg@users.noreply.github.com>
2025-08-13 15:53:25 +0300

    Add files via upload

---
+++ HW2/src/test/java/GBStandTests.java
@@ -0,0 +1,115 @@
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import pom.LoginPage;
+import pom.MainPage;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.module.Configuration;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.time.Duration;
+import java.util.Properties;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class GBStandTests {
+    static Properties prop = new Properties();
+    public WebDriver driver;
+    public WebDriverWait wait;
+    public static String gb_username;
+    public static String gb_password;
+    private LoginPage loginPage;
+    private MainPage mainPage;
+
+    @BeforeAll
+    static void initTests() throws IOException {
+        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
+        prop.load(new FileInputStream("src/test/resources/stand.properties"));
+        gb_username =  prop.getProperty("gb_username");
+        gb_password = prop.getProperty("gb_password");
+
+    }
+    @BeforeEach
+    public void setupTest(){
+        Selenide.open("http://test-stand.gb.ru/login");
+
+        driver = WebDriverRunner.getWebDriver();
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.get("http://test-stand.gb.ru/login");
+        loginPage = new LoginPage(driver, wait);
+
+    }
+
+
+    @AfterEach
+    public void tearDown(){
+        WebDriverRunner.closeWebDriver();
+    }
+
+
+    @Test
+    void loginTest(){
+        loginPage.login(gb_username, gb_password);
+        mainPage = new MainPage(driver, wait);
+        assertTrue(mainPage.getUsernameLabelText().contains(gb_username));
+    }
+
+    @Test
+    public void testAddingGroupOnMainPage() throws IOException {
+        loginPage.login(gb_username, gb_password);
+        mainPage = new MainPage(driver, wait);
+        assertTrue(mainPage.getUsernameLabelText().contains(gb_username));
+
+        String studentTestName = "Student " + System.currentTimeMillis();
+        mainPage.createStudent(studentTestName);
+        assertTrue(mainPage.waitAndGetNameByText(studentTestName).isDisplayed());
+        TakesScreenshot screenshotTaker = (TakesScreenshot) driver;
+        File screenshot = screenshotTaker.getScreenshotAs(OutputType.FILE);
+        File destination = new File("src\\test\\resources\\screenshot.png");
+        Files.copy(screenshot.toPath(), destination.toPath(), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    @Test
+    public void incorrectLoginTest(){
+        loginPage.loginWithOutUsernameAndPassword();
+        assertEquals("Invalid credentials.", loginPage.getErrorMassage());
+
+    }
+
+    @Test
+    public void trashButtonCorrectWork(){
+        loginPage.login(gb_username, gb_password);
+        mainPage = new MainPage(driver, wait);
+        assertTrue(mainPage.getUsernameLabelText().contains(gb_username));
+        String studentTestName = "Student " + System.currentTimeMillis();
+        mainPage.createStudent(studentTestName);
+        mainPage.closeCreateForm();
+        assertEquals("active", mainPage.getStatusWithName(studentTestName));
+        mainPage.clickTrashIconOnStudentWithName(studentTestName);
+        assertEquals("inactive", mainPage.getStatusWithName(studentTestName));
+        mainPage.clickRestoreFromTrashIconOnStudentWithName(studentTestName);
+        assertEquals("active", mainPage.getStatusWithName(studentTestName));
+    }
+
+
+
+
+
+
+}
+
